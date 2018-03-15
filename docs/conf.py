9034cbd07185426842ebb103e3500125ca665595
Shabda Raaj <shabda@agiliq.com>
2018-03-15 11:06:19 +0530

    Updated conf

--- docs/conf.py
+++ docs/conf.py
@@ -218,8 +218,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'BuildingAPIDjango.tex', u'Building API Django Documentation',
-   u'Shabda Raaj, Rakesh Vidya Chandra', 'manual'),
+  (master_doc, 'BuildingAPIDjango.tex', u'Building APIs with Django and Django Rest Framework',
+   u'Agiliq', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
